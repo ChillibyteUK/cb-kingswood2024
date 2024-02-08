edc65e1cb23ef46aa25bd597ccc8ec9f45f209fd
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-02-08 10:05:37 +0000

    Update cb_map.php

--- page-templates/blocks/cb_map.php
+++ page-templates/blocks/cb_map.php
@@ -1,7 +1,7 @@
 <section class="map">
     <div class="container-xl">
         <div class="row g-4">
-            <div class="col-md-6">
+            <div class="col-md-6 col-lg-4">
                 <h2 class="h3">Our Location</h2>
                 <ul class="fa-ul">
                     <li><span class="fa-li"><i class="fa-solid fa-map-marker-alt"></i></span> <?=get_field('contact_address','options')?></li>
@@ -13,7 +13,7 @@
                     <li><span class="fa-li"><i class="fa-solid fa-calendar"></i></span> <?=get_field('opening_hours','options')?></li>
                 </ul>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 col-lg-8">
                 <iframe src="<?=get_field('maps_url','options')?>" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
