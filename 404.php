af64baef71037630fd2a0e8932728bc707ecd108
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-02-08 17:10:13 +0000

    Update 404.php

--- 404.php
+++ 404.php
@@ -6,25 +6,21 @@ get_header();
 ?>
 <main id="main" class="padding-top">
 <?php
-$bg = wp_get_attachment_image_url( get_field('hero_image','options'), 'full' );
+$bg = '/wp-content/uploads/2024/02/shutters-get-in-touch.jpg';
 ?>    
 <!-- hero -->
-<section id="hero" class="hero d-flex align-items-center hero--default mb-0">
+<section id="hero" class="hero d-flex align-items-center hero--default mb-0" style="background-position:center;background-image: url(<?=$bg?>);">
     <div class="overlay"></div>
-    <div class="hero__inner container-xl">
-        <div class="row h-100">
-            <div class="col-lg-6 hero__content d-flex flex-column justify-content-center order-2 order-lg-1 py-5" data-aos="fade">
-                <h1 class="mb-4">404 - Page Not Found</h1>
-                <div class="hero__content fs-5 mb-4">We can't seem to find the page you're looking for</div>
-                <div class="hero__cta">
-                    <a class="btn btn-default--secondary mb-4" href="/">Return to Homepage</a>
-                </div>
-            </div>
-            <div class="col-lg-6 hero__image order-1 order-lg-2" style="background-image:url(<?=$bg?>)">
-            </div>
+    <div class="container-xl">
+        <div class="hero__pre-title">
+            404
         </div>
+        <h1 class="hero__title">Page not found</h1>
+        <div class="hero__content mb-4">
+            We can't seem to find the page you're looking for.
+        </div>
+        <a href="/" class="ms-0 button">Return to the <span>Homepage</span></a>
     </div>
-    <div class="overlay--bottom"></div>
 </section>
 </main>
 <?php
