09b3ca8ebb15baa50b98abe386d2f37d796f8634
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-02-08 16:03:36 +0000

    image cols position

--- page-templates/blocks/cb_image_cols.php
+++ page-templates/blocks/cb_image_cols.php
@@ -1,6 +1,17 @@
 <?php
 $cols = get_field('num_cols') == '4' ? 'image_cols__grid--four' : 'image_cols__grid--three';
 $classes = $block['className'] ?? 'py-5';
+
+$pos = get_field('image_position') ?? 'image_cols--center';
+
+if ($pos == 'top') {
+    $pos = 'image_cols--top';
+}
+if ($pos == 'bottom') {
+    $pos = 'image_cols--bottom';
+}
+
+
 ?>
 <section class="image_cols <?=$classes?>">
     <div class="container-xl">
@@ -12,7 +23,7 @@ $classes = $block['className'] ?? 'py-5';
             <a href="<?=wp_get_attachment_image_url($i, 'full')?>"
                 class="glightbox" data-gallery="gallery<?$r?>">
                 <img src="<?=wp_get_attachment_image_url($i, 'medium')?>"
-                    alt="image" class="d-block w-100" />
+                    alt="image" class="d-block w-100 <?=$pos?>" />
             </a>
             <?php
         }
