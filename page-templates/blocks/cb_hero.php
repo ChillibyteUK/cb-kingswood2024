1d7f6b80c18480473bc81d797cd94fcd9bf614d2
CheechRockwizard <136230172+LamcatUK@users.noreply.github.com>
2024-02-09 11:10:12 +0000

    hid hero indicators

--- page-templates/blocks/cb_hero.php
+++ page-templates/blocks/cb_hero.php
@@ -6,18 +6,20 @@
             while(have_rows('background')) {
                 the_row();
                 ?>
-                <div class="carousel-item <?=$active?>">
-                    <img src="<?=wp_get_attachment_image_url(get_sub_field('image'),'full')?>" class="d-block w-100" alt="">
-                </div>
-                <?php
+            <div class="carousel-item <?=$active?>">
+                <img src="<?=wp_get_attachment_image_url(get_sub_field('image'), 'full')?>"
+                    class="d-block w-100" alt="">
+            </div>
+            <?php
                 $active = '';
             }
             ?>
 
         </div>
-        
+
         <?php
     if (count(get_field('background')) > 1) {
+        /*
         ?>
     <div class="carousel-indicators">
         <?php
@@ -30,41 +32,45 @@
         }
         ?>
     </div>
-    <?php
+        <?php
+        */
     }
-    ?>
+            ?>
     </div>
-    
+
     <div class="overlay"></div>
 
     <div class="container-xl">
         <div class="hero__pre-title">
             <?=get_field('pre_title')?>
         </div>
-        <h1 class="hero__title"><?=get_field('title')?></h1>
+        <h1 class="hero__title">
+            <?=get_field('title')?></h1>
         <div class="hero__content">
             <?=get_field('content')?>
         </div>
         <?php
-        if (get_field('cta_1') || get_field('cta_2')) {
-            ?>
+                if (get_field('cta_1') || get_field('cta_2')) {
+                    ?>
         <div class="hero__ctas pt-4">
             <?php
-            if(get_field('cta_1')) {
-                ?>
-<a href="/contact/book-appointment/" class="hero__cta hero__cta--green"><i class="fa-regular fa-calendar-days"></i> Book <span>Appointment</span></a>
-                <?php
-            }
-            if(get_field('cta_2')) {
-                ?>
-<a href="/request-a-brochure/" class="hero__cta hero__cta--gold"><i class="fa-regular fa-newspaper"></i> Request <span>Brochure</span></a>
-                <?php
-            }
-            ?>
+                    if(get_field('cta_1')) {
+                        ?>
+            <a href="/contact/book-appointment/" class="hero__cta hero__cta--green"><i
+                    class="fa-regular fa-calendar-days"></i> Book <span>Appointment</span></a>
+            <?php
+                    }
+                    if(get_field('cta_2')) {
+                        ?>
+            <a href="/request-a-brochure/" class="hero__cta hero__cta--gold"><i class="fa-regular fa-newspaper"></i>
+                Request <span>Brochure</span></a>
+            <?php
+                    }
+                    ?>
         </div>
         <?php
-        }
-        ?>
+                }
+            ?>
     </div>
 
 </section>
