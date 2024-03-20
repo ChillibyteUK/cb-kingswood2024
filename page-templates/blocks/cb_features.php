47b617718de213fe760d1227c1cae637b45295e1
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-03-20 17:19:30 +0000

    British Made

--- page-templates/blocks/cb_features.php
+++ page-templates/blocks/cb_features.php
@@ -6,40 +6,42 @@
                     alt="">
                 <div class="features__pre-title">Free</div>
                 <div class="features__title">Home Visit</div>
-                <div class="features__content">Our team of experts will take you through every option available, so you
-                    can be confident you'll make the right choice.</div>
+                <div class="features__content">Our expert design team will visit your home, taking you through samples and solutions to find the right interior feature for your home.</div>
             </div>
             <div class="features__card">
                 <img src="<?=get_stylesheet_directory_uri()?>/img/icon-flexible.svg"
                     alt="">
                 <div class="features__pre-title">Flexible</div>
                 <div class="features__title">0% Interest Free</div>
-                <div class="features__content">Our team of experts will take you through every option available, so you
-                    can be confident you'll make the right choice.</div>
+                <div class="features__content">We offer an interest-free payment option to make the process as stress-free as possible, so you can make your dream home a reality.</div>
             </div>
             <div class="features__card">
                 <img src="<?=get_stylesheet_directory_uri()?>/img/icon-installation.svg"
                     alt="">
                 <div class="features__pre-title">Expert</div>
                 <div class="features__title">Installation</div>
-                <div class="features__content">Our team of experts will take you through every option available, so you
-                    can be confident you'll make the right choice.</div>
+                <div class="features__content">Our in-house fitters will complete the installation to your full satisfaction and always finish the job to the highest standard.</div>
             </div>
             <div class="features__card">
                 <img src="<?=get_stylesheet_directory_uri()?>/img/icon-bespoke.svg"
                     alt="">
                 <div class="features__pre-title">Bespoke</div>
                 <div class="features__title">Custom Fitted</div>
-                <div class="features__content">Our team of experts will take you through every option available, so you
-                    can be confident you'll make the right choice.</div>
+                <div class="features__content">All our products are made-to-measure for your home, working to the exact measurements of your space for the perfect fit.</div>
             </div>
             <div class="features__card">
                 <img src="<?=get_stylesheet_directory_uri()?>/img/icon-value.svg"
                     alt="">
                 <div class="features__pre-title">Best Value</div>
                 <div class="features__title">Price Promise</div>
-                <div class="features__content">Our team of experts will take you through every option available, so you
-                    can be confident you'll make the right choice.</div>
+                <div class="features__content">Offered across all our ranges, you’ll know you’ll be getting a competitive price for your quality products and installation.</div>
+            </div>
+            <div class="features__card">
+                <img src="<?=get_stylesheet_directory_uri()?>/img/icon-british.svg"
+                    alt="">
+                <div class="features__pre-title">Handcrafted</div>
+                <div class="features__title">British Made</div>
+                <div class="features__content">All our products are handcrafted in the UK using only the finest materials for a long-lasting, high-quality finish.</div>
             </div>
         </div>
         <a href="/about/why-choose-us/" class="button">Find out <span>more</span></a>
