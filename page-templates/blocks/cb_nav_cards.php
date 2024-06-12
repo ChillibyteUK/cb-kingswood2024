583b865fcb4b3844fb01632281545302d5c60a99
LamcatUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-06-12 16:58:18 +0100

    Update cb_nav_cards.php

--- page-templates/blocks/cb_nav_cards.php
+++ page-templates/blocks/cb_nav_cards.php
@@ -8,11 +8,12 @@ $classes = $block['className'] ?? null;
         while(have_rows('cards')) {
             the_row();
             $l = get_sub_field('link') ?? null;
+            $image_alt = get_post_meta(get_sub_field('image'), '_wp_attachment_image_alt', true) ?? null;
             ?>
             <div class="nav_cards__card">
                 <img class="nav_cards__image"
                     src="<?=wp_get_attachment_image_url(get_sub_field('image'), 'large')?>"
-                    alt="">
+                    alt="<?=$image_alt?>">
                 <div class="nav_cards__card-inner">
                     <h3 class="nav_cards__title">
                         <?=get_sub_field('title')?>
@@ -26,14 +27,14 @@ $classes = $block['className'] ?? null;
                     <a href="<?=$l['url']?>"
                         target="<?=$l['target']?>"
                         class="nav_cards__button"><?=html_entity_decode($l['title'])?></a>
-                        <?php
+                    <?php
                     }
-                    ?>
+            ?>
                 </div>
             </div>
             <?php
         }
-            ?>
+?>
         </div>
     </div>
 </section>
