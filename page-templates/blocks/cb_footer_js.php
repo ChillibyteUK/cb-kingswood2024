8f731a35bd6f3931b8bc5887052b99951934c842
LamcatUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-06-21 10:36:12 +0100

    footer script block

--- page-templates/blocks/cb_footer_js.php
+++ page-templates/blocks/cb_footer_js.php
@@ -1,4 +1,14 @@
 <?php
-add_action('wp_footer', function () {
-    get_field('script');
-}, 9999);
+global $acf_script;
+$acf_script = get_field('script');
+
+function output_acf_script()
+{
+    global $acf_script;
+    if (!empty($acf_script)) {
+        echo '<script type="text/javascript">' . $acf_script . '</script>';
+    } else {
+        echo '<!-- No script found in ACF field -->';
+    }
+}
+add_action('wp_footer', 'output_acf_script', 9999);
