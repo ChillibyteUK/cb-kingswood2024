2526be1b7714f7675bae14f8b82cc1aaac66d57a
ChillibyteUK <dan@chillibyte.co.uk>
2025-12-02 09:38:28 +0000

    single

--- single.php
+++ single.php
@@ -1,106 +1,122 @@
 <?php
-// Exit if accessed directly.
-defined('ABSPATH') || exit;
+/**
+ * The template for displaying all single posts
+ *
+ * @package cb-kingswood2024
+ */
+
+defined( 'ABSPATH' ) || exit;
+
 get_header();
-// $img = get_the_post_thumbnail_url(get_the_ID(), 'full');
-$thumbnail_id = get_post_thumbnail_id(get_the_ID());
-$alt_text = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+
+
+$thumbnail_id = get_post_thumbnail_id( get_the_ID() );
+$alt_text     = get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true );
 
 ?>
 <main id="main" class="blog">
     <?php
     $content = get_the_content();
-$blocks = parse_blocks($content);
-$sidebar = array();
-$after;
-?>
+	$blocks  = parse_blocks( $content );
+	$sidebar = array();
+	$after;
+	?>
     <section class="breadcrumbs container-xl">
         <?php
-if (function_exists('yoast_breadcrumb')) {
-    yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
-}
-?>
+		if ( function_exists( 'yoast_breadcrumb' ) ) {
+			yoast_breadcrumb( '<p id="breadcrumbs">', '</p>' );
+		}
+		?>
     </section>
     <div class="container-xl">
         <div class="row g-4 pb-4">
             <div class="col-lg-9 order-2">
-                <h1 class="blog__title"><?=get_the_title()?></h1>
-                <?=get_the_post_thumbnail(get_the_ID(), 'full', array('class' => 'blog__image', 'alt' => $alt_text))?>
+                <h1 class="blog__title"><?= esc_html( get_the_title() ); ?></h1>
+                <?=
+				get_the_post_thumbnail(
+					get_the_ID(),
+					'full',
+					array(
+						'class' => 'blog__image',
+						'alt'   => $alt_text,
+					)
+				);
+				?>
                 <?php
-        $count = estimate_reading_time_in_minutes(get_the_content(), 200, true, true);
-echo $count;
+        		$count = estimate_reading_time_in_minutes( get_the_content(), 200, true, true );
+				echo wp_kses_post( $count );
 
-foreach ($blocks as $block) {
-    if ($block['blockName'] == 'core/heading') {
-        if (!array_key_exists('level', $block['attrs'])) {
-            $heading = strip_tags($block['innerHTML']);
-            $id = acf_slugify($heading);
-            echo '<a id="' . $id . '" class="anchor"></a>';
-            $sidebar[$heading] = $id;
-        }
-    }
-    if ($block['blockName'] == 'acf/cb-text-image') {
-            $heading = " " . strip_tags($block['attrs']['data']['title']);
-            $id = acf_slugify($heading);
-            echo '<a id="' . $id . '" class="anchor"></a>';
-            $sidebar[$heading] = $id;
-    }
-    echo render_block($block);
-}
-?>
+				foreach ( $blocks as $block ) {
+					if ( 'core/heading' === $block['blockName'] ) {
+						if ( ! array_key_exists( 'level', $block['attrs'] ) ) {
+							$heading = wp_strip_all_tags( $block['innerHTML'] );
+							$anc_id  = sanitize_title( $heading );
+							echo '<a id="' . esc_attr( $anc_id ) . '" class="anchor"></a>';
+							$sidebar[ $heading ] = $anc_id;
+						}
+					}
+					if ( 'acf/cb-text-image' === $block['blockName'] ) {
+							$heading = ' ' . wp_strip_all_tags( $block['attrs']['data']['title'] );
+							$anc_id  = sanitize_title( $heading );
+							echo '<a id="' . esc_attr( $anc_id ) . '" class="anchor"></a>';
+							$sidebar[ $heading ] = $anc_id;
+					}
+					echo render_block( $block ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				}
+				?>
             </div>
             <div class="col-lg-3 order-1">
                 <?php
-    if ($sidebar) {
-        ?>
+    			if ( $sidebar ) {
+        			?>
                 <div class="sidebar">
                     <div class="h5 d-none d-lg-block">Quick Links</div>
                     <div class="h5 d-lg-none" data-bs-toggle="collapse" href="#links" role="button">Quick Links</div>
                     <div class="collapse d-lg-block" id="links">
                         <?php
-            foreach ($sidebar as $heading => $id) {
-                ?>
-                        <li><a
-                                href="#<?=$id?>"><?=$heading?></a>
-                        </li>
-                        <?php
-            }
-        ?>
+            			foreach ( $sidebar as $heading => $anc_id ) {
+                			?>
+                        <li><a href="#<?= esc_attr( $anc_id ); ?>"><?= esc_html( $heading ); ?></a></li>
+                        	<?php
+            			}
+        				?>
                     </div>
                 </div>
-                <?php
-    }
-?>
+                	<?php
+    			}
+				?>
             </div>
         </div>
         <section class="related pb-5">
             <h3>Related Posts</h3>
             <div class="row g-4">
                 <?php
-            $cats = get_the_category();
-$ids = wp_list_pluck($cats, 'term_id');
-$r = new WP_Query(array(
-    'category__in' => $ids,
-    'posts_per_page' => 4
-));
-while ($r->have_posts()) {
-    $r->the_post();
-    ?>
+				$cats = get_the_category();
+				$ids  = wp_list_pluck( $cats, 'term_id' );
+				$r    = new WP_Query(
+					array(
+						'category__in'   => $ids,
+						'posts_per_page' => 4,
+					)
+				);
+				while ( $r->have_posts() ) {
+	    			$r->the_post();
+    				?>
                 <div class="col-md-6 col-xl-3">
                     <a class="blog_card"
-                        href="<?=get_the_permalink()?>">
-                        <img src="<?=get_the_post_thumbnail_url(get_the_ID(), 'large')?>"
+                        href="<?= esc_url( get_the_permalink() ); ?>">
+                        <img src="<?= esc_url( get_the_post_thumbnail_url( get_the_ID(), 'large' ) ); ?>"
                             alt="" class="blog_card__image">
                         <div class="blog_card__content">
                             <h3 class="blog_card__title">
-                                <?=get_the_title()?>
+                                <?= esc_html( get_the_title() ); ?>
                             </h3>
                         </div>
                     </a>
                 </div>
-                <?php
-}
-?>
+                	<?php
+				}
+				?>
             </div>
         </section>
     </div>
